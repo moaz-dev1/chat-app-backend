705ccd9d8fd9e083f1543c946a7dcffc6249a8d3
Moaaz Mahrous <moaaz98@users.noreply.github.com>
2023-07-05 00:38:44 -0700

    db connection

---
+++ src/database/connection.ts
@@ -0,0 +1,14 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = new Pool({
+    host: process.env.DB_HOST!,
+    database: process.env.DB_Name!,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: +process.env.DB_PORT!,
+});
+
+export default pool;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import pool from "./database/connection";
 
 dotenv.config();
 
 const app = express();
+
+app.listen(process.env.PORT, function(){
+    pool.connect();
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
